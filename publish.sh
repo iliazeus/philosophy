016e271eed7291e17c181f13b4a306c466bbfea3
Ilia Pozdnyakov <ilia.pozdnyakov@ya.ru>
2018-06-11 12:10:08 +0700

    Add source commit info to published commits

--- publish.sh
+++ publish.sh
@@ -8,6 +8,6 @@ git checkout gh-pages
 cp ${TRAVIS_BUILD_DIR}/*.html .
 
 git add -A
-git commit -m "Build gh-pages"
+git commit -m "Build gh-pages from:" -m "$(git log HEAD^.. --oneline)"
 git push https://${GH_OAUTH_TOKEN}@github.com/${GH_USER_NAME}/${GH_PROJECT_NAME} 2>&1
 
